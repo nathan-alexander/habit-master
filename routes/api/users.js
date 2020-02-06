3c36dcfdb7b9a7b7db217f09601447568bf2faec
Nathan Alexander <hatenate@gmail.com>
2020-02-05 17:21:40 -0800

    added register user functionality to api/users

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator');
+const config = require('config');
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// @route  GET api/users
-// @desc   Test Route
+const User = require('../../models/User');
+
+// @route  POST api/users
+// @desc   Register User
 // @access Public
-router.get('/', (req, res) => res.send('User route'));
+router.post('/', [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
+], async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({errors: errors.array() })
+  }
+  const {name, email, password} = req.body;  
+  
+  try {
+      // See if user exists
+      let user = await User.findOne({email});
+      if (user) {
+          return res.status(400).json({errors:[{msg: 'User already exists'}]});
+      }
+      user = new User({
+          name,
+          email,
+          password
+      });
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      const payload = {
+          user: {
+              id: user.id
+          }
+      }
+      // Return jsonwebtoken
+      jwt.sign(
+            payload, 
+            config.get('jwtSecret'), 
+            {expiresIn: 3600}, 
+            (err, token) => {
+                if(err) throw err;
+                res.json({token});
+                }
+      );
+      
+  } catch (error) {
+      console.error(error.message);
+      return res.status(500).send('Server Error');
+  }
+  
+
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const connectDB = require('./config/db');
 const app = express();
 //Connect DB
 connectDB();
+//Init Middleware
+app.use(express.json({extended: false}));
 
 app.get('/', (req, res) => res.send('API Running'));
 
